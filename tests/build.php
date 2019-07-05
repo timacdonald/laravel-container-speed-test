c48eb55815356eaa89890de55e321884a9332782
Tim MacDonald <hello@timacdonald.me>
2019-07-05 11:04:42 +1000

    add route build script

--- tests/build.php
+++ tests/build.php
@@ -1,24 +1,83 @@
 <?php
 
-$containerTestStub = file_get_contents(__DIR__.'/container_test_stub.php');
-$withoutContainerTestStub = file_get_contents(__DIR__.'/without_container_test_stub.php');
-$methodStub = file_get_contents(__DIR__.'/method_stub.php');
+use Faker\Factory;
 
-$methods = '';
+require __DIR__.'/../vendor/autoload.php';
 
-for($i = 0; $i < 15; $i++) {
-    $methods .= str_replace('{method_name}', "test_{$i}", $methodStub);
+/**
+ * A bunch of helpers we use throughout the build process.
+ */
+
+$faker = Factory::create();
+
+$stub = function ($name) {
+    return file_get_contents(__DIR__."/{$name}_stub.php");
+};
+
+$create_test = function ($suite, $name, $content) {
+    file_put_contents(__DIR__."/{$suite}/{$name}.php", $content);
+};
+
+$create_route_file = function ($name, $content) {
+    file_put_contents(__DIR__."/../routes/{$name}.php", '<?php'.$content);
+};
+
+$build_routes = function ($properties) use ($stub) {
+    return array_reduce($properties, function ($body, $properties) use ($stub) {
+        return $body.array_reduce(array_keys($properties), function ($route, $property) use ($properties) {
+            return str_replace("{{$property}}", $properties[$property], $route);
+        }, $stub('route'));
+    }, '');
+};
+
+/**
+ * Generate the methods used in each of the Container and WithoutContainer
+ * test suite files.
+ */
+
+$body = '';
+
+for ($i = 0; $i < 15; $i++) {
+    $body .= str_replace('{method_name}', "test_{$i}", $stub('method'));
 }
+
+/**
+ * Generate the test files for both the Conatainer and WithoutContainer test suite.
+ */
 
 for ($i = 0; $i < 100; $i++) {
-    $className = "Test{$i}Test";
+    $container = str_replace('{className}', "Test{$i}Test", $stub('container_test'));
+    $without = str_replace('{className}', "Test{$i}Test", $stub('without_container_test'));
 
-    $containerTestContents = str_replace('{className}', "Test{$i}Test", $containerTestStub);
-    $withoutContainerTestContents = str_replace('{className}', "Test{$i}Test", $withoutContainerTestStub);
+    $container = str_replace('{body}', $body, $container);
+    $without = str_replace('{body}', $body, $without);
 
-    $containerTestContents = str_replace('{body}', $methods, $containerTestContents);
-    $withoutContainerTestContents = str_replace('{body}', $methods, $withoutContainerTestContents);
-
-    file_put_contents(__DIR__."/Container/{$className}.php", $containerTestContents);
-    file_put_contents(__DIR__."/WithoutContainer/{$className}.php", $withoutContainerTestContents);
+    $create_test('Container', "Test{$i}Test", $container);
+    $create_test('WithoutContainer', "Test{$i}Test", $without);
 }
+
+/**
+ * Generate the route properties to use when making the route files.
+ */
+
+$routes = [];
+
+for ($i = 0; $i < 75; $i++) {
+    $routes['web'][] = [
+        'url' => $faker->unique()->regexify('[a-z]{4,10}-[a-z]{4,10}\/{[a-z]{4,10}}\/[a-z]{4,10}'),
+        'name' => $faker->unique()->domainName,
+        'method' => $faker->randomElement(['get', 'post', 'patch', 'delete']),
+        'function' => $faker->randomElement(['index', 'create', 'edit', 'update', 'destroy']),
+        'controller' => str_replace(' ', '', $faker->unique()->jobTitle.'Controller'),
+    ];
+}
+
+$routes['api'] = array_map(function ($properties) {
+    return array_merge($properties, [
+        'url' => 'api/'.$properties['url'],
+        'name' => 'api.'.$properties['name'],
+    ]);
+}, $routes['web']);
+
+$create_route_file('web', $build_routes($routes['web']));
+$create_route_file('api', $build_routes($routes['api']));
---
+++ tests/route_stub.php
@@ -0,0 +1,6 @@
+
+
+Route::{method}('{url}', [
+    'as' => '{name}',
+    'uses' => '{controller}@{function}',
+]);
